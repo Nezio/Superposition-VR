1512a20c32d17fb780ce02affeeb50a9ba1c6600
Nezio <nemanjaz1994@gmail.com>
2020-05-12 22:52:29 +0200

    Add basic movement
    Added basic movement with character controller.

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public GameObject player;
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        player.transform.Rotate(Vector3.up * mouseX);
+
+        Debug.Log(mouseX);
+
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public CharacterController controller;
     public float movementSpeed = 2f;
 
     private Camera mainCamera;
 
-    // Start is called before the first frame update
     void Start()
     {
         mainCamera = Camera.main;
-}
+    }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(Tools.GetKeycode("W")))
-        {
-            transform.position +=  new Vector3(mainCamera.transform.forward.x, transform.forward.y, mainCamera.transform.forward.z) * Time.deltaTime * movementSpeed;
+        {            
+            Vector3 move = new Vector3(mainCamera.transform.forward.x, 0, mainCamera.transform.forward.z) * Time.deltaTime * movementSpeed;
+            controller.Move(move);
         }
 
     }

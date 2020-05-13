a2d39d8e508e94a1209ac7b34ef46ffb6dfeac79
Nezio <nemanjaz1994@gmail.com>
2020-05-14 00:06:45 +0200

    Add look down to move
    Added look down to move.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,13 +14,28 @@ public class PlayerController : MonoBehaviour
         mainCamera = Camera.main;
     }
 
+    private float maxX = 0;
+    private float maxY = 0;
+    private float maxZ = 0;
+
     void Update()
     {
-        if (Input.GetKey(Tools.GetKeycode("W")))
-        {            
-            Vector3 move = new Vector3(mainCamera.transform.forward.x, 0, mainCamera.transform.forward.z) * Time.deltaTime * movementSpeed;
-            controller.Move(move);
+        Debug.Log(mainCamera.transform.forward);
+        // look down to move
+        if(mainCamera.transform.eulerAngles.x >= 45f && mainCamera.transform.eulerAngles.x < 90f)
+        {
+            MoveForward();
         }
 
+        if (Input.GetKey(Tools.GetKeycode("W")) || Input.GetMouseButton(1))
+        { 
+            MoveForward();
+        }
+    }
+
+    private void MoveForward()
+    {
+        Vector3 move = new Vector3(mainCamera.transform.forward.x, 0, mainCamera.transform.forward.z).normalized * Time.deltaTime * movementSpeed;
+        controller.Move(move);
     }
 }

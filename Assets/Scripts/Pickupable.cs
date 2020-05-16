2244e5b2c089d90c9a7b9cc2bc55c792e54b5d48
Nezio <nemanjaz1994@gmail.com>
2020-05-16 17:27:24 +0200

    Add throw force
    Added throw force dependent on throw button held down time.

--- Assets/Scripts/Pickupable.cs
+++ Assets/Scripts/Pickupable.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Pickupable : MonoBehaviour
 {
+    public float throwForce = 5f;
+    public float maxThrowForce = 10f;
+
     private Camera mainCamera;
     private Transform hand;
     private Rigidbody rigidbody;
     private bool isHeld = false;
-    private float throwForce = 5f;
+    private float throwStartTime;
+    private bool isThrowing = false;
 
     private void Start()
     {
@@ -28,19 +32,29 @@ public class Pickupable : MonoBehaviour
 
     }
 
-    public void EventClick()
+    private void OnMouseDown()
     {
         Debug.Log("click");
 
-        if(isHeld)
+        if (isHeld)
         {
-            Throw();
+            // start throwing
+            throwStartTime = Time.time;
+            isThrowing = true;
         }
         else
         {
             PickUp();
         }
-        
+    }
+
+    private void OnMouseUp()
+    {
+        // throw inly if object is held and throwing has started
+        if(isHeld && isThrowing)
+        {
+            Throw();
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -55,19 +69,27 @@ public class Pickupable : MonoBehaviour
         transform.SetParent(hand);
         transform.localPosition = Vector3.zero;
         transform.localRotation = Quaternion.identity;
+        rigidbody.constraints = RigidbodyConstraints.FreezeAll;
     }
 
     private void Drop()
     {
         isHeld = false;
         rigidbody.useGravity = true;
+        rigidbody.constraints = RigidbodyConstraints.None;
         transform.SetParent(null);
     }
 
     private void Throw()
     {
         Drop();
-        rigidbody.AddForce(mainCamera.transform.forward*throwForce, ForceMode.Impulse);
+
+        float throwTime = Time.time - throwStartTime;
+        float forceMultiplier = Mathf.Min(throwTime * throwForce, maxThrowForce);
+        Debug.Log(forceMultiplier);
+        rigidbody.AddForce(mainCamera.transform.forward*forceMultiplier, ForceMode.Impulse);
+
+        isThrowing = false;
     }
 
     

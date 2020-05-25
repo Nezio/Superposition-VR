6f235c308a236a801e7c633eced103c03e81f8fc
Nezio <nemanjaz1994@gmail.com>
2020-05-26 00:20:43 +0200

    Add level builder pro and other fixes
    Added level builder pro.
    Improved main menu.
    Started level design.
    Updated door prefab to match the unit grid.
    Added sensor component.
    Fixed slow fall caused by the exit button.

---
+++ Assets/Scripts/Sensor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sensor : MonoBehaviour
+{
+    private Signal signal;
+
+    private void Start()
+    {
+        signal = transform.parent.gameObject.GetComponent<Signal>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            signal.SetSignal();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            signal.ResetSignal();
+        }
+    }
+
+
+}
